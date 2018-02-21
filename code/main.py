9cad24570ce47c9ca8469cc933cbd18001f89779
Sriharsha Sammeta <sriharsha4444@icloud.com>
2018-02-20 19:02:21 -0500

    adding error

--- code/main.py
+++ code/main.py
@@ -16,7 +16,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World (lets see how long a change takes III)!'
-
+    return "Hello, World (lets see how long a change takes III)!'
 if __name__ == '__main__':
     app.run(debug=True)
